5826a2dad23d51e64b7da69553706baefd2fa1ec
moutinho <josepedrovilelabravo@gmail.com>
2021-06-04 10:53:43 +0100

    Update  (Google Maps API)

--- app/src/main/java/com/example/grupo4_redesmoveis/MapsActivity.java
+++ app/src/main/java/com/example/grupo4_redesmoveis/MapsActivity.java
@@ -2,28 +2,56 @@ package com.example.grupo4_redesmoveis;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.grupo4_redesmoveis.ui.login.LoginFragment;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.grupo4_redesmoveis.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
     private ActivityMapsBinding binding;
+    private Button btn_start;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private boolean permissionDenied = false;
+    private boolean permissionDenied = false, running = false;;
+    private ArrayList<LatLng> percurso = new ArrayList<LatLng>();
+    Timer timer;
+    TimerTask timerTask;
+
+    //we are going to use a handler to be able to run in our TimerTask
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +59,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-
-        /*// Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
         enableMyLocation();
+
+        btn_start = this.findViewById(R.id.start_btn);
+        Polyline polyline2;
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btn_start.getText().equals("Iniciar")) {
+                    running = true;
+                    percurso.add(new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude()));
+                    btn_start.setText("Terminar");
+                    startTimer();
+
+                } else{ /** Terminar Corrida **/
+                    running = false;
+                    stoptimertask();
+                    percurso.add(new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude()));
+                    btn_start.setText("Iniciar");
+
+                    Polyline polyline2 = map.addPolyline(new PolylineOptions()
+                            .clickable(true)
+                            .addAll(percurso));
+                            polyline2.setWidth(20);
+                            polyline2.setColor(Color.BLUE);
+
+                    Fragment fragment = new Results_Fragment();
+                    FragmentManager fm = getSupportFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.replace(R.id.frameMaps, fragment);
+                    ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    ft.commit();
+                }
+
+            }
+        });
     }
+    public void startTimer() {
+        //set a new Timer
+        timer = new Timer();
+        //initialize the TimerTask's job
+        initializeTimerTask();
+        //schedule the timer, after the first 5s the TimerTask will run every 5s
+        timer.schedule(timerTask, 5000, 5000); //
+    }
+    public void stoptimertask() {
+        //stop the timer, if it's not already null
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+    public void initializeTimerTask() {
+        timerTask = new TimerTask() {
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        percurso.add(new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude()));
+                    }
+                });
+            }
+        };
+    }
+
     private void enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             if (map != null) {
                 map.setMyLocationEnabled(true);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng (41.48465170000002, -8.417248357670776), 16));  /** Trocar por Localizaçao atual aqui **/
             }
         } else {
             // Permission to access the location is missing. Show rationale and request permission
